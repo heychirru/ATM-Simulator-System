5ba732fce6b913759368fe346387dbb9e25e8e6a
Chiranjit Das <147528779+heychirru@users.noreply.github.com>
2026-09-12 20:00:10 +0530

    Harden transaction repository

--- src/ASimulatorSystem/backend/repository/TransactionRepository.java
+++ src/ASimulatorSystem/backend/repository/TransactionRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<BankTransaction, Long> {
-    List<BankTransaction> findTop20ByAccountIdOrderByCreatedAtDescIdDesc(Long accountId);
+    List<BankTransaction> findTop10ByAccountIdOrderByCreatedAtDescIdDesc(Long accountId);
 }
