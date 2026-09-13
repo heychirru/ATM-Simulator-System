0046268164f2c37bd8e77b8fe152f0ece400df0d
Chiranjit Das <147528779+heychirru@users.noreply.github.com>
2026-09-13 11:15:10 +0530

    Remove REST server entry point from desktop app

--- src/main/java/ASimulatorSystem/backend/AtmApplication.java
+++
@@ -1,9 +0,0 @@
-package ASimulatorSystem.backend;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class AtmApplication {
-    public static void main(String[] args) { SpringApplication.run(AtmApplication.class, args); }
-}
